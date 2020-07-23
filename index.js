f22217484455081b7571f2bd93a3df97ebb4dc45
Marcello <emyb@bellsouth.net>
2020-07-22 23:26:35 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.key)
